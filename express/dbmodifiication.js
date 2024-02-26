1d1c3d1a7533fc8109abb3042a7e004f0decc83e
peace akhaze <infinitypeandra017@gmail.com>
2024-02-26 14:18:29 +0100

    journey update

--- express/dbmodifiication.js
+++ express/dbmodifiication.js
@@ -27,10 +27,19 @@ app.post('/api/people',(req,res)=>{
 
 })
 //post methods inserts data, sends data into a database
+app.post('/api/poster/people',(req,res)=>{
+const {name}=req.body
+if(!name){
+  return  res.status(400).json({success:false, message:'please provide a name'})}
+else{
+  res.status(401).json({success:true, message:`please provide credentials`})
+}
+
+})
 app.post('/login',(req,res)=>
 //path is gotten from the form action
 { 
-/* console.log(req.body) */
+ /* console.log(req.params) */ 
 const {name}=req.body
 if(name){
   res.status(200).send(`welcome ${name}`)
@@ -39,8 +48,12 @@ else{
 res.status(401).send('please provide credentials')
 }
 })
-
-
+app.put('/api/people/:id',(req,res)=>{
+const{id}=req.params
+const{name}=req.body
+console.log(id, name)
+res.send(`hey ${name}, you have been slotted in space ${id} `)
+})
 
 app.listen(5000,()=>{
 console.log('server is listening on port 5000')
